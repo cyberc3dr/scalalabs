d6f4303199312ccc05954282fd25452fab3dbebd
cyberc3dr <geo3gamer@gmail.com>
2026-09-07 21:03:38 +0300

    scalalabs: init commit

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    scala
+    application
+    alias(libs.plugins.shadow)
+}
+
+scala {
+    scalaVersion = libs.versions.scala
+    zincVersion = libs.versions.zinc
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "ru.cyberc3dr.scalaapp.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.scala.library)
+    implementation(libs.guava)
+}
+
+tasks.withType<Jar> {
+    destinationDirectory = file("$rootDir/build")
+    archiveVersion = ""
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+    }
+}
+
+sourceSets.main {
+    scala.srcDir("src")
+    resources.srcDir("resources")
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java.toolchain.languageVersion = JavaLanguageVersion.of(21)
---
+++ src/ru/cyberc3dr/scalaapp/App.scala
@@ -0,0 +1,8 @@
+package ru.cyberc3dr.scalaapp
+
+object App:
+
+  def main(args: Array[String]): Unit =
+    println("Hello world!")
+
+end App
